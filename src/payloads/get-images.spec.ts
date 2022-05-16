255534090118e8d4dcbc6f938d976fa3bfcad5e7
Kai Krings <kai.krings@posteo.de>
2022-05-16 16:08:00 +0000

    Add unit tests for get-images

---
+++ src/payloads/get-images.spec.ts
@@ -0,0 +1,39 @@
+import { DiaryEntryDto } from '../types/diary-entry.dto';
+import { ImageDto } from '../types/image.dto';
+import { getDiaryEntries } from './get-diary-entries';
+import { getImages } from './get-images';
+
+describe('get-images', () => {
+  let images: ImageDto[];
+
+  beforeEach(() => {
+    images = getImages();
+  });
+
+  describe("images' IDs", () => {
+    let imageIds: Set<string>;
+
+    beforeEach(() => {
+      imageIds = new Set(images.map((image) => image.id));
+    });
+
+    it('should be unique', () => {
+      expect(imageIds.size).toEqual(images.length);
+    });
+  });
+
+  describe('#images', () => {
+    let diaryEntries: DiaryEntryDto[];
+
+    beforeEach(() => {
+      diaryEntries = getDiaryEntries();
+    });
+
+    it("should be equal to diary entries' images", () => {
+      expect(images).toEqual([
+        diaryEntries[1].images[0],
+        diaryEntries[2].images[0],
+      ]);
+    });
+  });
+});
