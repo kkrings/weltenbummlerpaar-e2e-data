47fe0f2e3c022c3d91bf6d6b1b537e3cdaaece5e
Kai Krings <kai.krings@posteo.de>
2022-05-16 15:47:18 +0000

    Add first unit tests

---
+++ src/payloads/get-diary-entries.spec.ts
@@ -0,0 +1,59 @@
+import { DiaryEntryDto } from '../types/diary-entry.dto';
+import { ImageDto } from '../types/image.dto';
+import { getDiaryEntries } from './get-diary-entries';
+
+describe('get-diary-entries', () => {
+  let diaryEntries: DiaryEntryDto[];
+
+  beforeEach(() => {
+    diaryEntries = getDiaryEntries();
+  });
+
+  describe("diary entries' IDs", () => {
+    let diaryEntryIds: Set<string>;
+
+    beforeEach(() => {
+      diaryEntryIds = new Set(diaryEntries.map((entry) => entry.id));
+    });
+
+    it('should be unique', () => {
+      expect(diaryEntryIds.size).toEqual(diaryEntries.length);
+    });
+  });
+
+  describe('#diaryEntries[1].images[0]', () => {
+    let diaryEntry: DiaryEntryDto;
+    let image: ImageDto;
+
+    beforeEach(() => {
+      diaryEntry = diaryEntries[1];
+      image = diaryEntry.images[0];
+    });
+
+    it("#diaryEntryId should be equal to diary entry's ID", () => {
+      expect(image.diaryEntryId).toEqual(diaryEntry.id);
+    });
+
+    it('#previewImage should be equal to image', () => {
+      expect(diaryEntry.previewImage).toEqual(image);
+    });
+  });
+
+  describe('#diaryEntries[2].images[0]', () => {
+    let diaryEntry: DiaryEntryDto;
+    let image: ImageDto;
+
+    beforeEach(() => {
+      diaryEntry = diaryEntries[2];
+      image = diaryEntry.images[0];
+    });
+
+    it("#diaryEntryId should be equal to diary entry's ID", () => {
+      expect(image.diaryEntryId).toEqual(diaryEntry.id);
+    });
+
+    it('#previewImage should be equal to image', () => {
+      expect(diaryEntry.previewImage).toEqual(image);
+    });
+  });
+});
