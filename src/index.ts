96dac6e053a0ba2094ed71437a3a1408a4f14ad8
Kai Krings <kai.krings@posteo.de>
2022-05-17 15:49:27 +0000

    Export all types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './database/setup';
 export * from './payloads/get-diary-entries';
 export * from './payloads/get-images';
+export * from './types/date-range.dto';
+export * from './types/diary-entry.dto';
+export * from './types/image.dto';
